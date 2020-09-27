f816b1cbad7a3dd456549f342b630fa8ca80fd44
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-09-28 00:41:18 +0700

    chore: sqlite db

--- config/database.js
+++ config/database.js
@@ -1,39 +1,17 @@
 module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env('DATABASE_PORT', 27017),
-            name: env('DATABASE_NAME', 'strapi'),
-            username: env('DATABASE_USERNAME', ''),
-            password: env('DATABASE_PASSWORD', ''),
-          },
-          options: {
-            ssl: false,
-          },
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
         },
       },
-    }
-  } else {
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'sqlite',
-            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-          },
-          options: {
-            useNullAsDefault: true,
-          },
-        },
-      },
-    }
+    },
   }
 };
