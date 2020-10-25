98ee7d260d394b08dcf1fe9075c0443b227d115e
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-10-26 03:15:11 +0700

    chore: setup env prod with postgres

--- config/database.js
+++ config/database.js
@@ -1,17 +1,57 @@
 module.exports = ({ env }) => {
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
+  if (env('NODE_ENV') === 'production') {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env('DATABASE_PORT', 27017),
+            name: env('DATABASE_NAME', 'strapi'),
+            username: env('DATABASE_USERNAME', ''),
+            password: env('DATABASE_PASSWORD', ''),
+          },
+          options: {
+            ssl: false,
+          },
         },
       },
-    },
+    }
+  } else {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
+      },
+    }
   }
 };
+
+// module.exports = ({ env }) => {
+//   return {
+//     defaultConnection: 'default',
+//     connections: {
+//       default: {
+//         connector: 'bookshelf',
+//         settings: {
+//           client: 'sqlite',
+//           filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//         },
+//         options: {
+//           useNullAsDefault: true,
+//         },
+//       },
+//     },
+//   }
+// };
