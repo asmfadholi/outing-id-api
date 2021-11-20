47a153f462c3209bf6e539c39295dcc195b74918
Mochammad Fadholi <market.fadholi@gmail.com>
2021-11-20 20:28:35 +0700

    chore: migrate gcp to do

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,14 +2,17 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
-        // host: env('DATABASE_HOST'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: {
+          ca: env("DATABASE_CA", ""),
+        },
       },
       options: {},
     },
--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,22 +1,13 @@
 module.exports = ({ env }) => ({
-  upload: {
-    provider: 'google-cloud-storage',
-    providerOptions: {
-        bucketName: env('BUCKET_NAME'),
-        publicFiles: false,
-        uniform: false,
-        basePath: '',
-    },
-  },
   email: {
     provider: 'smtp',
     providerOptions: {
       host: env('SMTP_HOST'), //SMTP Host
       port: env('SMTP_PORT')   , //SMTP Port
-      // secure: true,
+      
       username: env('SMTP_USERNAME'),
       password: env('SMTP_PASSWORD'),
-      // rejectUnauthorized: true,
+     
       requireTLS: true,
       connectionTimeout: 1,
     },
@@ -25,14 +16,5 @@ module.exports = ({ env }) => ({
       replyTo: env('EMAIL_REPLY_TO'),
     },
   },
-  // email: {
-  //   provider: 'sendgrid',
-  //   providerOptions: {
-  //     apiKey: env('SENDGRID_API_KEY'),
-  //   },
-  //   settings: {
-  //     defaultFrom: 'no-reply@outing.id',
-  //     defaultReplyTo: 'no-reply@outing.id',
-  //   },
-  // },
+  
 });
